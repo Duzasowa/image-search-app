11593eb5be3604a3f14bc826510689c2d067f30b
Duzasowa <harencko.vitaliy@gmail.com>
2023-01-24 15:01:40 +0100

    The function of showing photos on request and highlighting them to the user has been improved

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ImageShow from "./ImageShow";
+import "../style/ImageList.css";
 
-const ImageList = () => {
-  return <div>ImageList</div>;
+const ImageList = ({ images }) => {
+  const renderedImages = images.map((image) => {
+    return <ImageShow key={image.id} image={image} />;
+  });
+  return <div className="image-list">{renderedImages}</div>;
 };
 
 export default ImageList;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { useState } from "react";
+import "../style/SearchBar.css";
 
-const SearchBar = () => {
-  return <div>SearchBar</div>;
+const SearchBar = ({ onSubmit }) => {
+  const [term, setTerm] = useState("");
+
+  const handleFormClick = (event) => {
+    event.preventDefault();
+
+    onSubmit(term);
+  };
+
+  const handlerChange = (event) => {
+    setTerm(event.target.value);
+  };
+  return (
+    <div className="search-bar">
+      <form onSubmit={handleFormClick}>
+        <label>Enter Search term</label>
+        <input value={term} onChange={handlerChange} />
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
